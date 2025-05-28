1f2afb9b85008f062fac75e00f65122b5f9f85af
Ian Esteves <pk20lian@gmail.com>
2025-05-28 16:34:16 -0300

    authentication fix

--- app/bot/adapter.py
+++ app/bot/adapter.py
@@ -4,7 +4,7 @@ import os
 from botbuilder.core import BotFrameworkAdapter, BotFrameworkAdapterSettings
 
 adapter_settings = BotFrameworkAdapterSettings(
-    app_id=os.environ.get("MICROSOFT_APP_ID"),
-    app_password=os.environ.get("MICROSOFT_APP_PASSWORD")
+    app_id=os.environ.get("MICROSOFT_APP_ID", ""),
+    app_password=os.environ.get("MICROSOFT_APP_PASSWORD", "")
 )
 adapter = BotFrameworkAdapter(adapter_settings)
